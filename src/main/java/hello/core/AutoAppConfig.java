e7b3a94ac2512806b16eb452bb6fe01f939b8ca7
dayeongmaeng <dayeongmaeng@gmail.com>
2022-07-30 22:40:42 +0900

    @Component, @Autowired

---
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -0,0 +1,14 @@
+package hello.core;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+
+@Configuration
+@ComponentScan(
+        //예제 코드 유지를 위함
+        excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
+)
+public class AutoAppConfig {
+
+}
