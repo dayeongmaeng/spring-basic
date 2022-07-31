f1cc447d3d7a72c38025bc8a130f7864d8f9a6a4
dayeongmaeng <dayeongmaeng@gmail.com>
2022-07-31 22:07:56 +0900

    Filter

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -6,6 +6,10 @@ import org.springframework.context.annotation.FilterType;
 
 @Configuration
 @ComponentScan(
+//        지정하지 않으면, @ComponentScan이 붙은 hello.core 부터 하위 패키지 다 찾음
+//        권장 방법 : 최상단 (스프링부트 default)
+//        basePackages = "hello.core.member",
+//        basePackageClasses = AutoAppConfig.class,
         //예제 코드 유지를 위함
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
